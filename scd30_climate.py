fb18a2af8aeb27114e1a294e4c2e1b0e00852f48
Juan David Ruiz <juan.ruiz@ammp.io>
2020-06-05 10:36:28 +0200

    corrected co2 measurement

--- scd30_climate.py
+++ scd30_climate.py
@@ -48,6 +48,7 @@ if __name__ == "__main__":
         co2_raw[3] = raw_val[4]
         co2_val = bytearray(co2_raw)
         co2 = struct.unpack('>f', co2_val)[0]
+        co2 = co2*100
         print("CO2 = %f" % co2)
         time.sleep(.5)
 
