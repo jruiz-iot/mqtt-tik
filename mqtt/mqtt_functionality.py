2912f3c6b4937107dfd34cb57f42ad8280ea097a
Juan David Ruiz <juan.ruiz@ammp.io>
2020-06-05 11:52:50 +0200

    fixed f string

--- mqtt/mqtt_functionality.py
+++ mqtt/mqtt_functionality.py
@@ -1,11 +1,9 @@
-#!/usr/bin/env python3
-
 import paho.mqtt.client as mqtt
 
 # This is the Publisher
 def publish(measurement, value):
     client = mqtt.Client()
     client.connect("localhost",1883,60)
-    client.publish("air_sensor", f"{measurement}, site=office value={value}")
+    client.publish("air_sensor", "%s, site=office value=%s" % (measurement, value))
     client.disconnect()
 
