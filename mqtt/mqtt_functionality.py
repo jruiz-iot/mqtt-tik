80f82081558606fa2add66228b7c8639addc6f9e
Juan David Ruiz <juan.ruiz@ammp.io>
2020-06-05 16:16:31 +0200

    added last will 2

--- mqtt/mqtt_functionality.py
+++ mqtt/mqtt_functionality.py
@@ -3,7 +3,7 @@ import paho.mqtt.client as mqtt
 # This is the Publisher
 def publish(measurement, value):
     client = mqtt.Client()
-    client.will_set('air_sensor/status', 'LOST_CONNECTION', 0, False
+    client.will_set('air_sensor/status', 'LOST_CONNECTION', 0, False)
     client.connect("localhost",1883,60)
     client.publish("air_sensor", "%s,site=office value=%s" % (measurement, value))
     client.disconnect()
